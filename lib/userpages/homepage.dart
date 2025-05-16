1d7b80e14d36dd35e03e8f4f1d7745924eee7895
Kais Rafie <2371100@edu.vaniercollege.qc.ca>
2025-05-16 12:56:08 -0400

    Hot fix for home page

--- lib/userpages/homepage.dart
+++ lib/userpages/homepage.dart
@@ -220,7 +220,10 @@ class _HomePageState extends State<HomePage> {
                   const SizedBox(height: 50),
 
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (context) => SearchIncidentsPage()));
+                    },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xFF2F4F4F),
                       foregroundColor: Colors.white,
